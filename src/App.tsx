a6700462afdff988c7214fb657c610baa98e27a7
asif ahemmed <asifahemmed0@gmail.com>
2025-03-11 03:06:12 +0600

    createPost page created

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import { Navbar } from "./components/Navbar"
+import { Route, Routes } from "react-router";
+import { Navbar } from "./components/Navbar";
+import { CreatePostPage } from "./pages/CreatePostPage";
 
 function App() {
- 
-
   return (
-    <div>
-      <Navbar/>
+    <div className="min-h-screen bg-black text-gray-100 transition-opacity duration-700 pt-20">
+      <Navbar />
+      <div className="container mx-auto px-4 py-6">
+        <Routes>
+          <Route path="/create" element={<CreatePostPage />} />
+        </Routes>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
